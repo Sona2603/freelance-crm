5c1d9f4ab3d8f4dd5c37ac1f2e3cbe7896fc8c4d
Sona2603 <sonanagarajan076@gmail.com>
2026-06-28 11:07:22 +0530

    Update API URL for production

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -164,6 +164,16 @@ STATICFILES_STORAGE = (
     "whitenoise.storage.CompressedManifestStaticFilesStorage"
 )
 
-DATABASES["default"] = dj_database_url.config(
-    conn_max_age=600
-)
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.parse(
+            os.environ.get("DATABASE_URL")
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
