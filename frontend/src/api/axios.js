209be9a1ef0af33c8c620f5701348b072c2bf8ff
Sona2603 <sonanagarajan076@gmail.com>
2026-06-28 12:53:44 +0530

    Fix register API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:  "https://freelance-crm-kb58.onrender.com/api/",
+  baseURL: "https://freelance-crm-kb58.onrender.com/api/",
 });
 
 api.interceptors.request.use((config) => {
